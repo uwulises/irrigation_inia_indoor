f073d285367eca28fba2b05c5c10025e0db97798
uwulises <ulises.campodonico@ug.uchile.cl>
2022-12-17 17:01:26 -0300

    change library import

--- modules/indoor_class.py
+++ modules/indoor_class.py
@@ -1,8 +1,4 @@
 import time
-import sys
-import os
-#append phidget library folder
-sys.path.insert(0, os.path.abspath('../lib'))
 from Phidget22.Phidget import *
 from Phidget22.Devices.DigitalOutput import *
 
