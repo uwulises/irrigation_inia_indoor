40a0ee70dc140996094a4035b407040d24d4ff2f
uwulises <ulises.campodonico@ug.uchile.cl>
2022-12-17 21:46:25 -0300

    add some sensors

--- modules/indoor_basic.py
+++ modules/indoor_basic.py
@@ -5,19 +5,26 @@ Phidget = Simple_Phidget()
 
 def main():
     Phidget.begin()
-    time.sleep(2)
-    print("Estado valvulas")
-    print(Phidget.valve0_state)
-    print(Phidget.valve1_state)
-    time.sleep(1)
-    print("Encendiendo valvulas")
+    print("Abriendo valvulas")    
     Phidget.valve_1(True)
     Phidget.valve_0(True)
-    time.sleep(4)
-    print("Apagando valvulas")
+    time.sleep(2)
+    print("Cerrando valvulas")
     Phidget.valve_1(False)
     Phidget.valve_0(False)
-    time.sleep(10)
+    time.sleep(2)
+    print("Calling moist sensor")
+    i=0
+    while i<20:
+        print("Moist 0 sensor value: {}".format(Phidget.moist_sensor()))
+        print("Flow 0 sensor value: {}".format(Phidget.flow_0()))
+        print("Flow 1 sensor value: {}".format(Phidget.flow_1()))
+        print("Estado valvula 0 (False close; True open): {}".format(Phidget.valve0_state))
+        print("Estado valvula 0 (False close; True open): {}".format(Phidget.valve1_state))
+        time.sleep(0.2)
+        i=i+1
+    Phidget.stop()
+    print("STOP")
 main()
 
 
--- modules/indoor_class.py
+++ modules/indoor_class.py
@@ -1,25 +1,43 @@
 import time
 from Phidget22.Phidget import *
 from Phidget22.Devices.DigitalOutput import *
-
-
-
+from Phidget22.Devices.VoltageInput import *
 
 
 class Simple_Phidget:
     def __init__(self, valve0_state=False, valve1_state=False):
         self.valve0_state = valve0_state
         self.valve1_state = valve1_state
+        
         #Create your Phidget channels
+        
+        #Actuadores digitales
         self.relay_out0 = DigitalOutput()
         self.relay_out1 = DigitalOutput()
-        #Set addressing parameters to specify which channel to open (if any)
-        self.relay_out0.setChannel(0)
-        self.relay_out1.setChannel(1)
+
+        #Sensores
+        self.moist0 = VoltageInput()
+        self.flow0 = VoltageInput()
+        self.flow1 = VoltageInput()
+        self.thermopile = VoltageInput()
+        
+        #Canal de actuadores
+        self.relay_out0.setChannel(0) #Rele 0 en Digital Output 0
+        self.relay_out1.setChannel(1) #Rele 1 en Digital Output 1
+
+        #Canal de sensores
+        self.moist0.setChannel(0) #Sensor humedad en Analog Input 0
+        self.flow0.setChannel(1) #Sensor de flujo 0 en Analog Input 1
+        self.flow1.setChannel(2) #Sensor de flujo 1 en Analog Input 2
+        self.thermopile.setChannel(3) #Piranometro en Analog Input 3
+
         #Open your Phidgets and wait for attachment
         self.relay_out0.openWaitForAttachment(5000)
         self.relay_out1.openWaitForAttachment(5000)
-        
+        self.moist0.openWaitForAttachment(5000)
+        self.flow0.openWaitForAttachment(5000)
+        self.flow1.openWaitForAttachment(5000)
+        self.thermopile.openWaitForAttachment(5000)
 
     def valve_0(self, valve0_state):
         if valve0_state:
@@ -38,12 +56,28 @@ class Simple_Phidget:
             self.valve1_state=False
             self.relay_out1.setDutyCycle(1)
 
+    def moist_sensor(self):
+        return self.moist0.getVoltage()
+
+    def flow_0(self):
+        return self.flow0.getVoltage()
+
+    def flow_1(self):
+        return self.flow1.getVoltage()
+
+    def thermopile_sensor(self):
+        return self.thermopile.getVoltage()
+
     def begin(self):
         self.valve_0(False)
         self.valve_1(False)
         print("BEGIN")
         time.sleep(2)
 
-        
-
-    
+    def stop(self):
+        self.moist0.close()
+        self.flow0.close()
+        self.flow1.close()
+        self.relay_out0.close()
+        self.relay_out1.close()
+        self.thermopile.close()
