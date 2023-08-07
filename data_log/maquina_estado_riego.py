9848c5078e018768fc4dfb65e6decd76a1cf8993
uwulises <ulises.campodonico@ug.uchile.cl>
2023-08-07 16:57:40 -0400

    update argument on function add status log

--- data_log/maquina_estado_riego.py
+++ data_log/maquina_estado_riego.py
@@ -59,13 +59,15 @@ class InitState(State):
         print("Iniciando sistema de riego.")
         time.sleep(1)  #Espera de inicializacion
         init_time = ''
-        init_time = call_datetime()[0]
+        init_call_time = call_datetime()
+        init_time = init_call_time[0]
+        year_month_day = init_call_time[1]
         estado_valvula_0 = Phidget.valve0_state
         estado_valvula_1 = Phidget.valve1_state
         estado_humedad_0 = round(Phidget.moist_sensor0(),2)
         estado_humedad_1 = round(Phidget.moist_sensor1(),2)
         #log inicial del estado de sensores y actuadores
-        add_status_log_entry(State= 'Iniciando',tiempo_inicio=init_time, tiempo_actual= init_time , tiempo_termino=init_time, valve0_status=estado_valvula_0, valve1_status=estado_valvula_1, sensormoist0_value=estado_humedad_0, sensormoist1_value=estado_humedad_1)
+        add_status_log_entry(AAAA_MM_DD=year_month_day,State= 'Iniciando',tiempo_inicio=init_time, tiempo_actual= init_time , tiempo_termino=init_time, valve0_status=estado_valvula_0, valve1_status=estado_valvula_1, sensormoist0_value=estado_humedad_0, sensormoist1_value=estado_humedad_1)
         #ingreso al estado de espera de riego
         state_machine.set_state(WaitingState())
 
@@ -89,7 +91,10 @@ class WaitingState(State):
                 state_machine.set_state(ActiveState())
 
             else:
-                add_status_log_entry(State= 'EsperandoRiego', tiempo_actual=call_datetime()[0], valve0_status=estado_valvula_0, valve1_status=estado_valvula_1, sensormoist0_value=estado_humedad_0, sensormoist1_value=estado_humedad_1)
+                call_time = call_datetime()
+                actual_time = call_time[0]
+                year_month_day = call_time[1]
+                add_status_log_entry(AAAA_MM_DD=year_month_day,State= 'EsperandoRiego', tiempo_actual=actual_time, valve0_status=estado_valvula_0, valve1_status=estado_valvula_1, sensormoist0_value=estado_humedad_0, sensormoist1_value=estado_humedad_1)
 
 
                 
@@ -102,7 +107,9 @@ class ActiveState(State):
     def execute(self):
         print("Regando.")
         time.sleep(1)  # Wait for 1 second
-        before_irrigation = call_datetime()[0]
+        call_time = call_datetime()
+        before_irrigation_time = call_time[0]
+        year_month_day = call_time[1]
         timer = time.time()
         
         
@@ -122,7 +129,7 @@ class ActiveState(State):
         estado_humedad_0 = round(Phidget.moist_sensor0(),2)
         estado_humedad_1 = round(Phidget.moist_sensor1(),2)
         after_irrigation = call_datetime()[0]  
-        add_status_log_entry(State= 'Regando', tiempo_inicio=before_irrigation,tiempo_actual=call_datetime()[0], tiempo_termino=after_irrigation, valve0_status=estado_valvula_0, valve1_status=estado_valvula_1, sensor_caudal0_value=caudal_0, sensor_caudal1_value=caudal_1,sensormoist0_value=estado_humedad_0, sensormoist1_value=estado_humedad_1)
+        add_status_log_entry(AAAA_MM_DD=year_month_day,State= 'Regando', tiempo_inicio=before_irrigation_time,tiempo_actual=call_datetime()[0], tiempo_termino=after_irrigation, valve0_status=estado_valvula_0, valve1_status=estado_valvula_1, sensor_caudal0_value=caudal_0, sensor_caudal1_value=caudal_1,sensormoist0_value=estado_humedad_0, sensormoist1_value=estado_humedad_1)
         state_machine.set_state(WaitingState())
 
 # Create an instance of the state machine
