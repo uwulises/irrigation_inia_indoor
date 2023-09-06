364238d5da9dba251ab8583cfca80674810e31a1
uwulises <ulises.campodonico@ug.uchile.cl>
2023-09-06 01:46:38 -0300

    rebase to datalog only

--- modules/launcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# launcher.sh
-# navigate to home directory, then to this directoey, then execute python script, then back home
-
-cd /
-cd home/inia/phidget_indoor_project/modules
-sudo python set_valves.py
-cd /
--- modules/set_valves.py
+++
@@ -1,9 +0,0 @@
-from indoor_class import Simple_Phidget
-import time
-
-Phidget = Simple_Phidget()
-
-Phidget.valve_0(False)
-Phidget.valve_1(False)
-
-
