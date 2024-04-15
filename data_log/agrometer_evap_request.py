ebb802821c10cf4fbe406dd6a1f357aa5181eeb1
uwulises <ulises.campodonico@ug.uchile.cl>
2024-04-14 22:34:30 -0400

    edit double call

--- data_log/agrometer_evap_request.py
+++ data_log/agrometer_evap_request.py
@@ -10,8 +10,8 @@ from phidget_simple import SimplePhidget
 import time
 import numpy as np
 # Creacion de objeto phidget
-Phidget = SimplePhidget()
-Phidget.begin()  # Inicializacion minima
+# Phidget = SimplePhidget()
+# Phidget.begin()  # Inicializacion minima
 
 # Valores corte para humedad, revisar en el lugar
 HUMEDAD_MINIMA = 1.25
@@ -33,22 +33,3 @@ def evapotranspiracion():
     # call sensors
 
     return
-
-
-def init_state_entry():
-    global year_month_day
-    print("Iniciando sistema de riego.")
-    time.sleep(1)  # Espera de inicializacion
-    init_time = ''
-    print("Llamada a hora local")
-    init_call_time = call_datetime()
-    init_time = init_call_time[0]
-    year_month_day = init_call_time[1]
-    estado_valvula_0 = Phidget.valve0_state
-    estado_valvula_1 = Phidget.valve1_state
-    estado_humedad_0 = round(Phidget.moist_sensor0(), 2)
-    estado_humedad_1 = round(Phidget.moist_sensor1(), 2)
-    radiation = round(Phidget.pyr20_sensor(), 2)
-    # log inicial del estado de sensores y actuadores
-    add_status_log_entry(AAAA_MM_DD=year_month_day, State='Iniciando', tiempo_inicio=init_time, tiempo_actual=init_time, tiempo_termino=init_time,
-                         valve0_status=estado_valvula_0, valve1_status=estado_valvula_1, sensormoist0_value=estado_humedad_0, sensormoist1_value=estado_humedad_1, radiation_voltage=radiation, evapo_t_acum=EVAPOTRANSPIRACION_ACUMULADA)
