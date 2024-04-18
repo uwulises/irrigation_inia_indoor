df3cb02f28738e98715910f11ac4e44a7389daaa
uwulises <ulises.campodonico@ug.uchile.cl>
2024-04-18 13:14:47 -0400

    update if

--- data_log/atmos_sdi12.py
+++ data_log/atmos_sdi12.py
@@ -8,16 +8,8 @@ try:
         # Read a line from the serial port
         line = ser.readline().decode().strip()
         # Print the received line
-        print("Received:", line)
-        print("largo mensaje: ", len(line))
-        #example message
-        #Received: 183173, 0, 1, 4, 515, 1.5660001000, 38.2000010000, 0.2340000100, 98.8000110000
-        #if len is more than 70, then it is a valid message
-        if len(line)==77 or len(line)==78:
-            #take the last two values as float
-            print(line.split(","))
-            print(line.split(","))
-            # You can add your own processing logic here
+        print(line.split(","))
+        
         line = ""
         
 except KeyboardInterrupt:
