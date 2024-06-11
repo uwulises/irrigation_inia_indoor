2b27e958bd2e71aa75fcfe68df8997f8802ca24c
uwulises <ulises.campodonico@ug.uchile.cl>
2024-06-11 11:33:24 -0400

    debug

--- data_log/lisimetro.py
+++ data_log/lisimetro.py
@@ -15,10 +15,16 @@ try:
         load_cell.send_command("MEAS\n")
         time.sleep(20)
         # Read a line from the serial port
-        line = load_cell.read()
-        msg = line.split(",")
+        #Decode this line and take each value [b'SumaV1V2V3V4,19347.48,6381.76,5840.10,3131.46,3994.16\r\n']
+        msg = load_cell.read()
+        #decode
+        msg = msg.decode("utf-8")
+        #split the string
+        msg = msg.split(",")
         print(msg)
+
         print("len: ",len(msg))
+        
         if len(msg) == 5:
             measure = {"Peso total":msg[1],"valor1":msg[2],"valor2":msg[3],"valor3":msg[4],"valor4":msg[5]}
             #save data message as json, or edit the file
