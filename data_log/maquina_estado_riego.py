fe3aa873888364457fd3bd11741e5aa76a03d2f3
Ulises C <52766555+uwulises@users.noreply.github.com>
2024-07-09 10:24:05 -0400

    remove error log

--- data_log/maquina_estado_riego.py
+++ data_log/maquina_estado_riego.py
@@ -58,12 +58,12 @@ class StateMachine:
                 print(f"Error occurred: {str(e)}")
                 print("Restarting the state machine...")
                 self.state = InitState()  # Reset the state machine to initial state
-                # write a text file with the error and the time
-                with open('log/error_log.txt', 'a') as file:
-                    file.write(
-                        f"Error occurred: {str(e)} at {call_datetime()[0]}\n")
-                    # close the file
-                    file.close()
+                # # write a text file with the error and the time
+                # with open('log/error_log.txt', 'a') as file:
+                #     file.write(
+                #         f"Error occurred: {str(e)} at {call_datetime()[0]}\n")
+                #     # close the file
+                #     file.close()
 
 
 class State:
