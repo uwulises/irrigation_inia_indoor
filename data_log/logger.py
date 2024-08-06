5654da6667c25bc04ef4fb9ef1ab06b445fe0c4a
uwulises <ulises.campodonico@ug.uchile.cl>
2024-08-05 21:55:26 -0400

    add methods for json format

--- data_log/logger.py
+++ data_log/logger.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 # Funcion para guardar datos de funcionamiento, considera tiempos de inicio y final, estado de sensores y valvulas
 
 
@@ -23,10 +24,49 @@ def add_status_log_entry(AAAA_MM_DD='2023-00-00', State='', tiempo_inicio='', ti
     # Save the DataFrame to the CSV file
     datalog.to_csv(
         'log/invernadero_log_csv_{}.csv'.format(AAAA_MM_DD), index=False)
-    #datalog.to_excel('log/invernadero_log_excel_{}.xlsx'.format(AAAA_MM_DD))
+    # datalog.to_excel('log/invernadero_log_excel_{}.xlsx'.format(AAAA_MM_DD))
 
     # clean new entry with empty dataframe
     new_entry = None
+
+
+def add_entry(AAAA_MM_DD='2023-00-00', State='', tiempo_inicio='', tiempo_actual='', tiempo_termino='', valve0_status=False, valve1_status=False, sensor_caudal0_value=0.0, sensor_caudal1_value=0.0, sensormoist0_value=0.0, sensormoist1_value=0.0, radiation_voltage=0, evapo_t_acum=0.0, humedad=0, temperatura=0, lisimetro=0):
+    data = {
+        "State": State,
+        "Initial Time": tiempo_inicio,
+        "Tiempo Actual": tiempo_actual,
+        "Final Time": tiempo_termino,
+        "valve0_status": valve0_status,
+        "valve1_status": valve1_status,
+        "Sensor caudal 0": sensor_caudal0_value,
+        "Sensor caudal 1": sensor_caudal1_value,
+        "Sensor humedad 0": sensormoist0_value,
+        "Sensor humedad 1": sensormoist1_value,
+        "Radiacion [V]": radiation_voltage,
+        "Evapotranspiracion acumulada": evapo_t_acum,
+        "Humedad": humedad,
+        "Temperatura C": temperatura,
+        "Lisimetro": lisimetro
+    }
+    #load the json data
+    try:
+        with open('log/invernadero_log.json', 'r') as loadfile:
+            data_load = json.load(loadfile)
+    except FileNotFoundError:
+        data_load = []
+    
+    data_load.append(data)
+    with open('log/invernadero_log.json', 'w') as dumpfile:
+        json.dump(data_load, dumpfile)
+    
+    
+
+def get_actual_time():
+    with open('log/invernadero_log.json', 'r') as loadfile:
+        data_log = json.load(loadfile)
+    last_tiempo_actual = data_log[-1]['Tiempo Actual']
+    print(last_tiempo_actual)
+    return last_tiempo_actual
 
 
 def get_tiempo_actual_csv(AAAA_MM_DD='2024-00-00'):
