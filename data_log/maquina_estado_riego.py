be44fb9648e865bf5dd4f4978cb68843418d32cd
uwulises <ulises.campodonico@ug.uchile.cl>
2024-08-05 21:57:30 -0400

    update phidget stuff

--- data_log/maquina_estado_riego.py
+++ data_log/maquina_estado_riego.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "/log"))
 import time
 from time_zone_request import call_datetime, check_log_time_variable
 # Funciones para crear y agregar informacion en .csv y .xlsx
-from logger import add_status_log_entry, get_tiempo_actual_csv
+from logger import add_status_log_entry, get_tiempo_actual_csv, add_entry, get_actual_time
 #from agrometer_evap_request import init_state_entry
 # Clase de PhidgetInterfaceKit 8/8/8 w/6 Port Hub
 # Objeto Phidget cuenta con acciones I/O
@@ -25,8 +25,7 @@ EVAPOTRANSPIRACION_ACUMULADA = 0
 
 
 # Creacion de objeto phidget
-Phidget = SimplePhidget()
-Phidget.begin()  # Inicializacion minima
+Phidget = SimplePhidget() # Inicializacion minima
 
 '''Maquina de estado
 Revisa el estado de humedad en cada lado del invernadero
@@ -90,6 +89,7 @@ class InitState(State):
         init_call_time = call_datetime()
         init_time = init_call_time[0]
         year_month_day = init_call_time[1]
+        Phidget.begin() # Inicializacion de phidget
         estado_valvula_0 = Phidget.valve0_state
         estado_valvula_1 = Phidget.valve1_state
         estado_humedad_0 = round(Phidget.moist_sensor0(), 2)
@@ -108,10 +108,12 @@ class InitState(State):
             
         json_file.close()
         json2_file.close()
+        Phidget.stop()
         # log inicial del estado de sensores y actuadores
-        add_status_log_entry(AAAA_MM_DD=year_month_day, State='Iniciando', tiempo_inicio=init_time, tiempo_actual=init_time, tiempo_termino=init_time,
+        #add_status_log_entry(AAAA_MM_DD=year_month_day, State='Iniciando', tiempo_inicio=init_time, tiempo_actual=init_time, tiempo_termino=init_time,
+         #                   valve0_status=estado_valvula_0, valve1_status=estado_valvula_1, sensormoist0_value=estado_humedad_0, sensormoist1_value=estado_humedad_1, radiation_voltage=radiation, evapo_t_acum=EVAPOTRANSPIRACION_ACUMULADA, humedad=humedad, temperatura=temperatura, lisimetro=suma_total)
+        add_entry(AAAA_MM_DD=year_month_day, State='Iniciando', tiempo_inicio=init_time, tiempo_actual=init_time, tiempo_termino=init_time,
                             valve0_status=estado_valvula_0, valve1_status=estado_valvula_1, sensormoist0_value=estado_humedad_0, sensormoist1_value=estado_humedad_1, radiation_voltage=radiation, evapo_t_acum=EVAPOTRANSPIRACION_ACUMULADA, humedad=humedad, temperatura=temperatura, lisimetro=suma_total)
-
         # ingreso al estado de espera de riego
         state_machine.set_state(WaitingState())
 
@@ -125,23 +127,25 @@ class WaitingState(State):
         print("Esperando para regar, por evapotranspiracion o humedad")
         # Revisa cada 60seg el estado de los sensores de humedad
         time.sleep(60)
-        estado_valvula_0 = Phidget.valve0_state
-        estado_valvula_1 = Phidget.valve1_state
-        estado_humedad_0 = round(Phidget.moist_sensor0(), 2)
-        estado_humedad_1 = round(Phidget.moist_sensor1(), 2)
-        estado_radiacion = round(Phidget.pyr20_sensor(), 2)
-        with open('atmos.json') as json_file:
-            data = json.load(json_file)
-            humedad = data["Humedad"]
-            temperatura = data["Temperatura"]
-        json_file.close()
-        with open('lisimetro.json') as json2_file:
-            lisimetro_meas = json.load(json2_file)
-            suma_total=lisimetro_meas["Peso total"]
-        json2_file.close()
-
         # Si pasan >10min, check -> True
-        if check_log_time_variable(get_tiempo_actual_csv(year_month_day)):
+        if check_log_time_variable(get_actual_time()):
+        #if check_log_time_variable(get_tiempo_actual_csv(year_month_day)):
+            Phidget.begin()
+            estado_valvula_0 = Phidget.valve0_state
+            estado_valvula_1 = Phidget.valve1_state
+            estado_humedad_0 = round(Phidget.moist_sensor0(), 2)
+            estado_humedad_1 = round(Phidget.moist_sensor1(), 2)
+            estado_radiacion = round(Phidget.pyr20_sensor(), 2)
+            with open('atmos.json') as json_file:
+                data = json.load(json_file)
+                humedad = data["Humedad"]
+                temperatura = data["Temperatura"]
+            json_file.close()
+            with open('lisimetro.json') as json2_file:
+                lisimetro_meas = json.load(json2_file)
+                suma_total=lisimetro_meas["Peso total"]
+            json2_file.close()
+            Phidget.stop()
             # Si la condicion de humedad se cumple ingresa a estado de riego
             if (estado_humedad_0 < HUMEDAD_MINIMA and estado_humedad_0 > 0.0):
                 state_machine.set_state(ActiveState())
@@ -166,7 +170,7 @@ class ActiveState(State):
         before_irrigation_time = call_time[0]
         year_month_day = call_time[1]
         timer = time.time()
-
+        Phidget.begin() # Inicializacion de phidget
         while (time.time() - timer < 30.0):
 
             Phidget.valve_1(True)
@@ -188,6 +192,7 @@ class ActiveState(State):
         estado_humedad_1 = round(Phidget.moist_sensor1(), 2)
         after_irrigation = call_datetime()[0]
         estado_radiacion = round(Phidget.pyr20_sensor(), 2)
+        Phidget.stop()
         add_status_log_entry(AAAA_MM_DD=year_month_day, State='Regando', tiempo_inicio=before_irrigation_time, tiempo_actual=call_datetime()[
                              0], tiempo_termino=after_irrigation, valve0_status=estado_valvula_0, valve1_status=estado_valvula_1, sensor_caudal0_value=caudal_0, sensor_caudal1_value=caudal_1, sensormoist0_value=estado_humedad_0, sensormoist1_value=estado_humedad_1, radiation_voltage=estado_radiacion)
         state_machine.set_state(WaitingState())
