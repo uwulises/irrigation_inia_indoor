9973e07160f5990d68b902e438fa69761a0fe142
Ulises C <52766555+uwulises@users.noreply.github.com>
2024-08-13 12:12:46 -0400

    format

--- data_log/lisimetro.py
+++ data_log/lisimetro.py
@@ -14,17 +14,12 @@ try:
     while True:
         # Read a line from the serial port
         peso = load_cell.read_lisimetro()
-        if peso > 0:
-            kg= round(float(peso)*2.1e-5,2)
-            measure = {"Peso total":kg}
-            #save data message as json, or edit the file
-            with open('lisimetro.json', 'w') as f:
-                json.dump(measure, f)
-            #close the file
-            f.close()
-        peso = ""
-        measure = ""
-        time.sleep(5)
+        registro={"Peso total":peso}
+        with open('lisimetro.json', 'w') as f:
+            json.dump(registro, f)
+        #close the file
+        f.close()
+        registro = ""
         
 except KeyboardInterrupt:
     # If user interrupts, close the serial port
